66ea010a82eed5a521806569a14ee22b6df2d5a0
Victoria Kochetova <vikah8u@gmail.com>
2024-12-16 16:53:18 +0300

    KS-1-2 create variables

---
+++ src/main/kotlin/lessons/lesson_1_task_2.kt
@@ -0,0 +1,16 @@
+package org.example.lessons
+
+fun main() {
+    val countOfOrders = 75
+    val thankYouMessage = "Спасибо за покупку!"
+
+    println(countOfOrders)
+    println(thankYouMessage)
+
+    var countOfEmployees = 2000
+//  println(countOfEmployees)
+
+    countOfEmployees -= 1
+    println(countOfEmployees)
+
+}
