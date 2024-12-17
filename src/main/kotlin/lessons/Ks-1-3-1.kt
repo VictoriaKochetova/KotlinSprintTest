9fe460f868fd4a5b34232d993d8318e9f9b37b3f
Victoria Kochetova <vikah8u@gmail.com>
2024-12-17 20:03:22 +0300

    KS-1-3 create variables

---
+++ src/main/kotlin/lessons/Ks-1-3-1.kt
@@ -0,0 +1,17 @@
+package org.example.lessons
+
+fun main() {
+    val year = 1961
+    var hours = "09"
+    var minutes = "07"
+
+    println(year)
+    println(hours)
+    println(minutes)
+
+    hours = 10
+    minutes = 55
+
+    println("$hours:$minutes")
+
+}
