71325ca81352a27b319dc3175396c8a878155fc4
Victoria Kochetova <vikah8u@gmail.com>
2024-12-17 21:15:44 +0300

    KS-1-5 calculating average score

---
+++ src/main/kotlin/lessons_2/KS-2-1.kt
@@ -0,0 +1,14 @@
+package org.example.lessons_2
+
+fun main() {
+    val student_1 = 3
+    val student_2 = 4
+    val student_3 = 3
+    val student_4 = 5
+
+    val averageScore = (student_1 + student_2 + student_3 + student_4) / 4.0
+
+    println(String.format("%.2f", averageScore))
+
+
+}
